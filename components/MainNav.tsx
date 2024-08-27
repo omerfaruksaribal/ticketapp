10401f14b0b0b63865df163cd20a206d0e4ed480
omerfaruksaribal <omerfaruk_saribal@hotmail.com>
2024-08-27 10:41:08 +0300

    Structure Completed

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -2,10 +2,16 @@ import Link from 'next/link';
 
 const MainNav = () => {
   return (
-    <div className="">
-      <Link href="/">Main Page</Link>
-      <Link href="/tickets">Tickets</Link>
-      <Link href="/users">Users</Link>
+    <div className="flex justify-between">
+      <div className="flex items-center gap-4">
+        <Link href="/">Main Page</Link>
+        <Link href="/tickets">Tickets</Link>
+        <Link href="/users">Users</Link>
+      </div>
+      <div className="flex items-center gap-4">
+        <Link href="/">Logout</Link>
+        <Link href="/">Dark</Link>
+      </div>
     </div>
   );
 };
