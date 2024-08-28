e31d5a2f9a1236a5f46d278f39bdc5d317739f85
omerfaruksaribal <omerfaruk_saribal@hotmail.com>
2024-08-28 15:43:59 +0300

    Pagination and order added

--- app/tickets/DataTable.tsx
+++ app/tickets/DataTable.tsx
@@ -11,61 +11,91 @@ import {
 import { Ticket } from '@prisma/client';
 import Link from 'next/link';
 import React from 'react';
+import { ArrowDown } from 'lucide-react';
+import { SearchParams } from './page';
 
 interface Props {
   tickets: Ticket[];
+  searchParams: SearchParams;
 }
 
-const DataTable = ({ tickets }: Props) => {
+const DataTable = ({ tickets, searchParams }: Props) => {
   return (
     <div className="w-full mt-5">
       <div className="rounded-md sm:border">
         <Table>
           <TableHeader>
             <TableRow>
-              <TableHead>Title</TableHead>
               <TableHead>
-                <div className="flex justify-center items-center">Status</div>
+                <Link href={{ query: { ...searchParams, orderBy: 'title' } }}>
+                  Title
+                </Link>
+                {'title' === searchParams.orderBy && (
+                  <ArrowDown className="inline p-1" />
+                )}
               </TableHead>
               <TableHead>
-                <div className="flex justify-center">Priority</div>
+                <div className="flex justify-center">
+                  <Link
+                    href={{ query: { ...searchParams, orderBy: 'status' } }}
+                  >
+                    Status
+                  </Link>
+                  {'status' === searchParams.orderBy && (
+                    <ArrowDown className="inline p-1" />
+                  )}
+                </div>
               </TableHead>
-              <TableHead>Created At</TableHead>
+              <TableHead>
+                <div className="flex justify-center">
+                  <Link
+                    href={{ query: { ...searchParams, orderBy: 'priority' } }}
+                  >
+                    Priority
+                  </Link>
+                  {'priority' === searchParams.orderBy && (
+                    <ArrowDown className="inline p-1" />
+                  )}
+                </div>
+              </TableHead>
+              <TableHead>
+                <Link
+                  href={{ query: { ...searchParams, orderBy: 'createdAt' } }}
+                >
+                  Created At
+                </Link>
+                {'createdAt' === searchParams.orderBy && (
+                  <ArrowDown className="inline p-1" />
+                )}
+              </TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             {tickets
               ? tickets.map((ticket) => (
                   <TableRow key={ticket.id} data-href="/">
-                    <TableCell className="flex justify-start">
-                      {ticket.title}
+                    <TableCell>
+                      <Link href={`/tickets/${ticket.id}`}>{ticket.title}</Link>
                     </TableCell>
                     <TableCell>
                       <div className="flex justify-center">
                         <TicketStatusBadge status={ticket.status} />
                       </div>
                     </TableCell>
-
                     <TableCell>
                       <div className="flex justify-center">
                         <TicketPriority priority={ticket.priority} />
                       </div>
                     </TableCell>
-                    <TableCell className="flex justify-between items-center">
-                      {ticket.createdAt.toLocaleDateString('tr-TR', {
+                    <TableCell>
+                      {ticket.createdAt.toLocaleDateString('en-US', {
                         year: '2-digit',
                         month: '2-digit',
                         day: '2-digit',
                         hour: 'numeric',
                         minute: '2-digit',
-                        hour12: false,
+                        hour12: true,
                       })}
-                      <Link
-                        href={`/tickets/${ticket.id}`}
-                        className="ml-4 px-3 py-1 text-sm text-black bg-red-600 rounded-lg hover:bg-red-400 cursor-pointer"
-                      >
-                        View Details
-                      </Link>
                     </TableCell>
                   </TableRow>
                 ))
--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -3,19 +3,65 @@ import prisma from '@/prisma/db';
 import DataTable from './DataTable';
 import Link from 'next/link';
 import { buttonVariants } from '@/components/ui/button';
+import Pagination from '@/components/Pagination';
+import { Status, Ticket } from '@prisma/client';
 
-const Tickets = async () => {
-  const tickets = await prisma.ticket.findMany();
+export interface SearchParams {
+  status: Status;
+  page: string;
+  orderBy: keyof Ticket;
+}
+
+const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
+  const pageSize = 10;
+  const page = parseInt(searchParams.page) || 1;
+
+  const orderBy = searchParams.orderBy ? searchParams.orderBy : 'createdAt';
+
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  let where = {};
+
+  if (status) {
+    where = {
+      status,
+    };
+  } else {
+    where = {
+      NOT: [{ status: 'CLOSED' as Status }],
+    };
+  }
+
+  const ticketCount = await prisma.ticket.count({ where });
+
+  const tickets = await prisma.ticket.findMany({
+    where,
+    orderBy: {
+      [orderBy]: 'desc',
+    },
+    take: pageSize,
+    skip: (page - 1) * pageSize,
+  });
 
   return (
     <div>
-      <Link
-        href="/tickets/new"
-        className={buttonVariants({ variant: 'default' })}
-      >
-        Create New Ticket
-      </Link>
-      <DataTable tickets={tickets} />
+      <div className="flex gap-2">
+        <Link
+          href="/tickets/new"
+          className={buttonVariants({ variant: 'default' })}
+        >
+          Create New Ticket
+        </Link>
+      </div>
+      <DataTable tickets={tickets} searchParams={searchParams} />
+      <Pagination
+        itemCount={ticketCount}
+        pageSize={pageSize}
+        currentPage={page}
+      />
     </div>
   );
 };
