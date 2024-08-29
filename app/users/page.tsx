2d5499291e0e807418d8ccd9fe5f364fad1b5d31
omerfaruksaribal <omerfaruk_saribal@hotmail.com>
2024-08-29 11:20:48 +0300

    User Table

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,14 @@
 import UserForm from '@/components/UserForm';
+import DataTableSimple from './data-table-simple';
+import prisma from '@/prisma/db';
 
-const Users = () => {
+const Users = async () => {
+  const users = await prisma.user.findMany({});
+
   return (
     <div>
       <UserForm />
+      <DataTableSimple users={users} />
     </div>
   );
 };
